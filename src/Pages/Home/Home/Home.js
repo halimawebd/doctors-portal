2862fcef4f50170d8b2a730349ae4242bba27736
halimawebd <halimawebd@gmail.com>
2022-12-20 13:10:40 +0600

    home part

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,26 +1,26 @@
-// import React from 'react';
-// import Services from '../Services/Services';
-// import Banner from './Banner/Banner';
-// import Contact from './Contact/Contact';
-// import Exceptional from './Exceptional/Exceptional';
-// import InfoCards from './InfoCards/InfoCards';
-// import MakeAppoinment from './MakeAppoinment/MakeAppoinment';
-// import Testimonial from './Testimonial/Testimonial';
+import React from 'react';
+import Services from '../Services/Services';
+import Banner from './Banner/Banner';
+import Contact from './Contact/Contact';
+import Exceptional from './Exceptional/Exceptional';
+import InfoCards from './InfoCards/InfoCards';
+import MakeAppoinment from './MakeAppoinment/MakeAppoinment';
+import Testimonial from './Testimonial/Testimonial';
 
 
-// const Home = () => {
-//     return (
-//         <div className='mx-5'>
-//             <Banner></Banner>
-//             <InfoCards></InfoCards>
-//             <Services></Services>
-//             <Exceptional></Exceptional>
-//             <MakeAppoinment></MakeAppoinment>
-//             <Testimonial></Testimonial>
-//            <Contact></Contact>
+const Home = () => {
+    return (
+        <div className='mx-5'>
+            <Banner></Banner>
+            <InfoCards></InfoCards>
+            <Services></Services>
+            <Exceptional></Exceptional>
+            <MakeAppoinment></MakeAppoinment>
+            <Testimonial></Testimonial>
+           <Contact></Contact>
           
-//     </div>
-//     );
-// };
+    </div>
+    );
+};
 
-// export default Home;
+export default Home;
