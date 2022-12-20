4febaeca49f9ac06b711c3bfb0fddda3d7dd5f35
halimawebd <halimawebd@gmail.com>
2022-12-20 13:04:59 +0600

    appoinment part

--- src/Pages/Appoinment/Appoinment/Appoinment.js
+++ src/Pages/Appoinment/Appoinment/Appoinment.js
@@ -1,20 +1,20 @@
-// import React, { useState } from 'react';
-// import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
-// import AvailableAppoinments from '../AvailableAppoinment/AvailableAppoinments';
+import React, { useState } from 'react';
+import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
+import AvailableAppoinments from '../AvailableAppoinment/AvailableAppoinments';
 
-// const Appoinment = () => {
-//   const [selectedDate, setSelectedDate] = useState(new Date())
-//     return (
-//         <div>
-//           <AppointmentBanner 
-//           selectedDate={selectedDate}
-//           setSelectedDate={setSelectedDate}
-//           ></AppointmentBanner>
-//           <AvailableAppoinments
-//             selectedDate={selectedDate}
-//           ></AvailableAppoinments>
-//         </div>
-//     );
-// };
+const Appoinment = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date())
+    return (
+        <div>
+          <AppointmentBanner 
+          selectedDate={selectedDate}
+          setSelectedDate={setSelectedDate}
+          ></AppointmentBanner>
+          <AvailableAppoinments
+            selectedDate={selectedDate}
+          ></AvailableAppoinments>
+        </div>
+    );
+};
 
-// export default Appoinment;
+export default Appoinment;
